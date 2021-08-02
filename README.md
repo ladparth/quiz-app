a118acf2439a53d20d83361a81101df3382d6df6
Parth Lad <parthlad3915@gmail.com>
2021-08-02 12:38:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple quiz website created using HTML, CSS and JavaScript, which generates questions and options from JS Object and validates answers on client side keeping track of correct answers.
---
+++ script.js
@@ -0,0 +1,123 @@
+const quizData = [
+    {
+        question: 'If soccer is called football in England, what is American football called in England?',
+        a:'American football',
+        b:'comball',
+        c:'Handball',
+        d:'Touchdown',
+        answer: 'c'
+    },
+    {
+        question: 'What is the most used programming language?',
+        a:'JavaScript',
+        b:'Python',
+        c:'Java',
+        d:'C#',
+        answer: 'c'
+    },
+    {
+        question: 'Who is the best player in basketball history?',
+        a:'Jordon',
+        b:'Kobe',
+        c:'LeBrone',
+        d:'Shakil',
+        answer: 'a'
+    },
+    {
+        question: 'Who is the best player in football?',
+        a:'Messi',
+        b:'Ronaldo',
+        c:'Aguro',
+        d:'Salah',
+        answer: 'b'
+    }
+]
+
+const quiz = document.getElementById('quiz');
+
+const questionEle = document.getElementById('question');
+const a_text = document.getElementById('a_text');  
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit'); 
+const error = document.querySelector('.error');
+
+//Initialize quiz question & score
+let currentQuiz = 0;
+let score = 0;
+
+//Generate Quiz Qustions and Options from quizData array
+
+function loadQuiz(){
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEle.innerHTML = currentQuizData.question;
+
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+
+    //unCheckRadios();
+
+}
+//Loading the first question on to DOM
+loadQuiz();
+
+//Next Button 
+submitBtn.addEventListener('click', () =>{
+    //Store selected answer
+    const answer = getSelectedAnswer();
+    //store correct answer from the object
+    const currentQuizData = quizData[currentQuiz];
+    const correctAns = currentQuizData.answer;
+
+    //check if the answer was selected or not
+    if(answer){
+        error.style.display = 'none';
+        //Check answer is correct or not, if correct increment the score
+        if(answer === correctAns){
+            score++;
+        }
+        //Increment current question counter to load next question on to the DOM
+        currentQuiz++;
+
+        //Check if the current question counter is less than or equal to total question stored in the object.
+
+        if(currentQuiz<quizData.length){
+            loadQuiz();
+            //change the button text to Submit for last question only
+            changeBtnText();
+        }else{
+
+            //Show Result
+            quiz.innerHTML = `
+                <h2 class = "result">You answered ${score}/${quizData.length} questions correctly </h2>
+
+                <button onclick="location.reload()">Restart</button>`
+        }
+
+    }else{
+        error.style.display = 'block';
+    }
+    
+});
+
+function getSelectedAnswer(){
+    const answersEles = document.querySelectorAll('.answer');
+    let answer = undefined;
+    answersEles.forEach((answerEle) =>{
+        if(answerEle.checked){
+            answer = answerEle.id;
+        }
+        answerEle.checked = false;
+    });
+    return answer; 
+}
+function changeBtnText(){
+
+    if(currentQuiz === (quizData.length -1)){
+        submitBtn.innerText = 'Submit'
+    }
+}
